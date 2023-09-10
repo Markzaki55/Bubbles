0c57119165b613c1d9476205d49d1b07bb640060
mark zaki <120737045+Markzaki55@users.noreply.github.com>
2023-09-10 22:31:00 +0300

    added pop animation

--- Assets/LB1.cs
+++ Assets/LB1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
 
+
 interface Ipopable{
   ObjectPool<GameObject> ObjectPool { get; set; }
   void setpool(ObjectPool<GameObject> pool);
@@ -10,7 +11,7 @@ interface Ipopable{
     
 }
    
-public class LB1 : MonoBehaviour,Ipopable
+public class LB1 : bubble, Ipopable 
 {
    public ObjectPool<GameObject> ObjectPool { get; set; }
     PoppableObjectSpawner pool;
@@ -19,12 +20,45 @@ public class LB1 : MonoBehaviour,Ipopable
 
     public void pop()
     {
-       ObjectPool.Release(this.gameObject);
-        
+
+        StartCoroutine(popping(this.gameObject));
+       }
+    private IEnumerator popping(GameObject obj)
+    {
+        Vector3 originalScale = obj.transform.localScale;
+        Vector3 targetScale = Vector3.zero;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < 1)
+        {
+            float t = elapsedTime / 1;
+            obj.transform.localScale = Vector3.Lerp(originalScale, targetScale, t);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        obj.transform.localScale = targetScale;
+        obj.SetActive(false);
+        obj.transform.localScale = originalScale; 
+        base.pop(ObjectPool);
+
+
     }
 
     public void setpool(ObjectPool<GameObject> pool)
     {
         ObjectPool = pool;
     }
+}
+
+
+
+public class bubble: MonoBehaviour{ 
+
+
+ public  virtual void pop(UnityEngine.Pool.ObjectPool<GameObject> pool)
+    {
+        pool.Release(this.gameObject);
+    }
+
 }
