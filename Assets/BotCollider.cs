d59a6ed331e69de11738a8a379404f7523cef383
mark zaki <120737045+Markzaki55@users.noreply.github.com>
2023-09-13 03:01:53 +0300

    added gameover Panel Ui

--- Assets/BotCollider.cs
+++ Assets/BotCollider.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Pool;
 
 public class BotCollider : MonoBehaviour
 {
+    int misscounter;
+    [SerializeField] TextMeshProUGUI misscounterT;
    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("bubble"))
         {
             collision.gameObject.GetComponent<Ipopable>().ObjectPool.Release(collision.gameObject);
+            misscounter++;
+            misscounterT.text = "Miss :"+ misscounter;
         }
     }
 
